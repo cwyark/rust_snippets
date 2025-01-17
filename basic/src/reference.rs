60188348c27b1913e737a9c0f8afb0917d79cca8
YuSheng <chester@cwyark.me>
2025-01-17 17:02:43 +0800

    update reference

---
+++ basic/src/reference.rs
@@ -0,0 +1,17 @@
+fn calculate_len(s: &String) -> usize {
+    let text = s;
+    text.len()
+}
+
+fn change_str(s: &mut String) {
+    s.push_str(", world");
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+    let length = calculate_len(&s);
+    println!("s size is {}", length);
+    println!("s is {}", s);
+    change_str(&mut s);
+    println!("modified s is {}", s);
+}
